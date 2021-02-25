9bee80c8504c05b5e84284428511b399349364bc
artamonovoleg <ArtamonovAV2003@gmail.com>
2021-02-25 21:19:15 +0300

    Add files via upload

---
+++ Window.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <string>
+#include <GLFW/glfw3.h>
+
+struct WindowProps
+{
+    int width, height;
+    std::string title;
+};
+
+class Window
+{
+    private:
+        int m_Width, m_Height;
+        GLFWwindow* m_Window;
+    public:
+
+
+        Window(const WindowProps& props)
+            : m_Width(props.width), m_Height(props.height)
+        {
+            m_Window = glfwCreateWindow(m_Width, m_Height, props.title.c_str(), nullptr, nullptr);
+        }
+
+        ~Window()
+        {
+            glfwDestroyWindow(m_Window);
+        }
+
+        bool ShouldClose()
+        {
+            return glfwWindowShouldClose(m_Window);
+        }
+};
