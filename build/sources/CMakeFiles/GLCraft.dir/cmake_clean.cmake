6810e2ece729c2e61c37e0cefb31d6f9785bf683
Artamonov Oleg <artamonovoleg@Artamonovs-iMac-Pro.local>
2021-02-26 21:01:53 +0300

    ok

---
+++ build/sources/CMakeFiles/GLCraft.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "../GLCraft"
+  "../GLCraft.pdb"
+  "CMakeFiles/GLCraft.dir/Engine.cpp.o"
+  "CMakeFiles/GLCraft.dir/EventSystem.cpp.o"
+  "CMakeFiles/GLCraft.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GLCraft.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
