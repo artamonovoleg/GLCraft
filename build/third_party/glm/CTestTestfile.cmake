7205ae3c5a9aa14d2643a2ecd9a1ce9d96a8a3a5
artamonovoleg <artamonovav2003@gmail.com>
2021-02-28 00:27:28 +0300

    Test

--- build/third_party/glm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/artamonovoleg/Documents/GLCraft/third_party/glm
-# Build directory: /Users/artamonovoleg/Documents/GLCraft/build/third_party/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
--- build/third_party/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/artamonovoleg/Documents/GLCraft/third_party/glm/glm
-# Build directory: /Users/artamonovoleg/Documents/GLCraft/build/third_party/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
